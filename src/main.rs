1d6036aee2dbc680c72453a86ac29c52be0eb997
fihry <fihrylara@gmail.com>
2026-02-19 13:08:25 +0000

    Wire main to run single-threaded event loop

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,13 @@ mod core;
 mod http;
 
 use config::loader::ConfigLoader;
-use connection::stream::HandleConnection;
-use mio::{ Events, Interest, Poll, Token };
-use mio::net::{ TcpListener };
-use std::io;
-use std::net::{ AddrParseError, SocketAddr };
-use std::thread;
+use core::event_loop::EventLoop;
 
 fn main() -> Result<(), String> {
-    // Load configuration
-    macro_rules! hey {
-    ($address:expr) => {
-        println!("address: {}", $address);
-        println!("===================================");
-    };
-}
-
     let config = ConfigLoader::load("application.conf")?;
     config::validator::ConfigValidator::validate(&config)?;
-    for server in config.servers {
-        for port in server.ports {
-            let address: SocketAddr = format!("{}:{}", server.host, port)
-                .parse()
-                .map_err(|e: AddrParseError| e.to_string())?;
 
-            hey!(address);
-
-            thread::spawn(move || {
-                let listener = TcpListener::bind(address).unwrap();
-                println!("Listening on {}", address);
-
-                loop {
-                    match listener.accept() {
-                        Ok((stream, addr)) => {
-                            println!("Connection from {}", addr);
-                            let _ = HandleConnection::handle_connection(stream);
-                        }
-                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                            // Nothing to accept right now
-                            std::thread::sleep(std::time::Duration::from_millis(50));
-                        }
-                        Err(e) => {
-                            eprintln!("accept error: {}", e);
-                            break;
-                        }
-                    }
-                }
-
-            });
-        }
-    }
-
-    // prevent main from exiting
-    loop {
-        thread::park();
-    }
+    let mut event_loop = EventLoop::new(&config).map_err(|err| err.to_string())?;
+    event_loop.run().map_err(|err| err.to_string())?;
+    Ok(())
 }
