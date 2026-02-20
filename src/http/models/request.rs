68b28644428261b1f3077701c9a339ec77ccab30
fihry <fihrylara@gmail.com>
2026-02-20 13:34:22 +0000

    Define HTTP request model

--- src/http/models/request.rs
+++ src/http/models/request.rs
@@ -1,8 +1,23 @@
-// use crate::http::models::headers::Headers;
-//
-// pub struct Request {
-//     pub method: Method,
-//     pub path: String,
-//     pub headers: Headers,
-//     pub body: Option<Vec<u8>>,
-// }
+use crate::http::models::headers::Headers;
+use crate::http::models::method::Method;
+
+#[derive(Debug, Clone)]
+pub struct Request {
+    pub method: Method,
+    pub path: String,
+    pub version: String,
+    pub headers: Headers,
+    pub body: Vec<u8>,
+}
+
+impl Request {
+    pub fn new(method: Method, path: String, version: String, headers: Headers, body: Vec<u8>) -> Self {
+        Self {
+            method,
+            path,
+            version,
+            headers,
+            body,
+        }
+    }
+}
