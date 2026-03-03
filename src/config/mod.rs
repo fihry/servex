a482da3162fcfc449e643afc434b8ccaa15c0532
ibrahim el harraq <ielharra>
2026-03-03 11:50:41 +0000

    update teh config module

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -5,16 +5,4 @@ pub mod parser;
 pub mod loader;
 pub mod validator;
 pub mod server;
-
-// Re-export commonly used types
-pub use models::{
-    ServerConfig,
-    GlobalConfig,
-    Route,
-    CgiConfig,
-    Redirect,
-};
-
-pub use loader::ConfigLoader;
-pub use parser::IniParser;
-pub use validator::ConfigValidator;
+pub use models::Route;
